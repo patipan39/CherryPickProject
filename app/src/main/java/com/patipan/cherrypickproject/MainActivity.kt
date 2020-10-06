362de0409e3f6e48e46a41d33ad7e013cebe38f4
Patipan Injai <ford@appsynth.net>
2020-10-06 17:43:57 +0700

    remove set content

--- app/src/main/java/com/patipan/cherrypickproject/MainActivity.kt
+++ app/src/main/java/com/patipan/cherrypickproject/MainActivity.kt
@@ -4,7 +4,5 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
 
-    }
 }
